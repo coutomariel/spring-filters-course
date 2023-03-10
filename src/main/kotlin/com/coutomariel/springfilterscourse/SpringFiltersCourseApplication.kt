2a0284c477513807d9480ee0d35ed9932aa94e13
mariel <coutomariel@gmail.com>
2023-03-10 10:06:27 -0300

    Initial commit

---
+++ src/main/kotlin/com/coutomariel/springfilterscourse/SpringFiltersCourseApplication.kt
@@ -0,0 +1,11 @@
+package com.coutomariel.springfilterscourse
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringFiltersCourseApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringFiltersCourseApplication>(*args)
+}
